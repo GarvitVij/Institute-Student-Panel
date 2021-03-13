11d65be1597e4759b1d610c97a67d27678321118
garvit2126 <garvit2126@users.noreply.github.com>
2021-03-14 04:20:37 +0530

    Fixed Notices

--- src/container/Notices/notices.js
+++ src/container/Notices/notices.js
@@ -1,85 +1,38 @@
 import React, {Component} from 'react';
-// import Appbar from '../../component/UI/Appbar/Appbar';
 import classes from './notices.module.css';
-// import Drawer from '../../component/UI/Drawer/Drawer';
 import PaperDesign from '../../component/UI/Paper/Paper';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
+import axios from '../../axios'
+import Accordion from '../../component/UI/Accordion/Accordion'
+import { Typography } from '@material-ui/core';
 
 class Notices extends Component {
-    // state = {
-    //     isDrawerOpen: false,
-    // }
-  
-    
-    // onDrawerOpenHandler = () => {
-    //   this.setState({isDrawerOpen: true})
-    // }
-  
-    // onDrawerCloseHandler = () => {
-    //   this.setState({isDrawerOpen: false})
-    // }
+
+    state={
+        notices: []
+    }
+
+    componentDidMount(){
+        if(this.state.notices.length === 0){
+            axios.get('/api/student/get/notices', {withCredentials: true})
+            .then(res => {console.log(res);this.setState({notices: res.data.notices})})
+            .catch(err => console.log(err))
+        }
+      
+    }
 
     render () {
         return (
             <div  className={classes.Layout}>
-              <PaperDesign> 
-                <PaperDesign >
-                <Box border={3}  borderRadius={8}>
-                  <PaperDesign>
-                      <Box border={1} borderRadius={8}>
-                        <Typography variant="h6" gutterBottom>
-                             Kindly, Pay the Exam fees before 13/05/2021,
-                             else you have to pay late fee also.
-                        </Typography>
-                      </Box>
-
-                      <Box border={1} borderRadius={8}>
-                        <Typography variant="h6" gutterBottom>
-                              Exam will be held on 25/05/2021,
-                              Best of luck for your exams.                   
-                        </Typography>
-                      </Box>
-
-                      <Box border={1} borderRadius={8}>
-                        <Typography variant="h6" gutterBottom>
-                             Collect your admit card from 
-                             examination cell before 25/05/2021
-                        </Typography>
-                      </Box>
-
-                      <Box border={1} borderRadius={8}>
-                        <Typography variant="h6" gutterBottom>
-                             Kindly submit your all books,
-                             to library to take your admit card.                  
-                        </Typography>
-                      </Box>
-
-                      <Box border={1} borderRadius={8}>
-                        <Typography variant="h6" gutterBottom>
-                             Your next semester will start
-                             from first week of August                    
-                        </Typography>
-                      </Box>
-
-                      <Box border={1} borderRadius={8}>
-                        <Typography variant="h6" gutterBottom>
-                             Pay your next semester fee as 
-                             soon as possible               
-                        </Typography>
-                      </Box>
-
-                      <Box border={1} borderRadius={8}>
-                        <Typography variant="h6" gutterBottom>
-                              Exam will be held on 25/05/2021,
-                              Best of luck for your exams.                   
-                        </Typography>
-                      </Box>
-                    
-                   </PaperDesign>
-                   </Box>
+                <PaperDesign extraStyles={{padding: '1%'}}>
+                <Typography align="center" variant="h2">Notices</Typography>
+                <div className={classes.Notices}>
+                {this.state.notices.map((notice,index)=> (
+                    <div className={classes.Accordion}>
+                    <Accordion panel={index} key={index} heading={notice.title} description={notice.desc}/>
+                    </div>
+                    ))}
+                </div>
                 </PaperDesign>
-              </PaperDesign> 
             </div>
         )
     }
